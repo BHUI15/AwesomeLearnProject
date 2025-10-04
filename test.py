d3602f8fd1e637bacaf75c41963918f5040c130c
BHUI15 <157424814+BHUI15@users.noreply.github.com>
2025-10-04 17:53:18 +0100

    Update test.py
    add if statement

--- test.py
+++ test.py
@@ -1,5 +1,13 @@
 print("Hello World!")
 for i in range(5):
     print(i)
+# Add if statement
+age = 12
+if age > 18:
+    print("Your are an adult.")
+elif age < 2:
+    print("You are a toddler.")
+else:
+    print("You are still a child. And you can not drive nor drive a car.")
     
 
